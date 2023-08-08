5f40fc2f17d404399268de0107b9b8013bc60b55
connectviral <91738676+connectviral@users.noreply.github.com>
2023-08-07 23:03:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll(".section");
+    const navLinks = document.querySelectorAll("nav a");
+
+    // Show/hide back to top button based on scroll position
+    document.addEventListener("scroll", function () {
+        const backToTopBtn = document.getElementById("backToTopBtn");
+        if (window.scrollY > 200) {
+            backToTopBtn.classList.add("visible");
+        } else {
+            backToTopBtn.classList.remove("visible");
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            const targetSection = document.querySelector(link.getAttribute("href"));
+
+            sections.forEach((section) => {
+                section.classList.remove("highlight");
+            });
+
+            targetSection.classList.add("highlight");
+
+            navLinks.forEach((link) => {
+                link.classList.remove("active");
+            });
+
+            link.classList.add("active");
+
+            // Scroll to the target section with smooth animation
+            const scrollOptions = {
+                top: targetSection.offsetTop,
+                behavior: "smooth",
+            };
+            window.scrollTo(scrollOptions);
+
+            // Remove the highlight class after 3 seconds
+            setTimeout(() => {
+                targetSection.classList.remove("highlight");
+            }, 3000);
+        });
+    });
+});
